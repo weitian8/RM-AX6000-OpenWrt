ae88ab0ba91a94e58e65488d9eb34e1b594410c3
Weitian <31085999+weitian8@users.noreply.github.com>
2023-04-22 13:16:14 +0800

    Update diy-part1.sh

--- padavanonly/Redmi-AX6000/diy-part1.sh
+++ padavanonly/Redmi-AX6000/diy-part1.sh
@@ -23,6 +23,6 @@
 rm -rf feeds/luci/applications/luci-app-passwall
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# git pull
-# ./scripts/feeds update -a
-# ./scripts/feeds install -a
+git pull
+./scripts/feeds update -a
+./scripts/feeds install -a
